eae9cd9e72d938b7ee24085bfc30526cd21405e1
JoyMbugua <mbuguajoy52@gmail.com>
2021-08-27 11:06:43 +0300

    defined character class

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Hero:
+    '''
+      class to define character objects
+    '''
+    def __init__(self, id, name, description):
+      self.id = id
+      self.name = name
+      self.description = description
