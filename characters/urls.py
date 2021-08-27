b84bdfb2e035e75ccf11a6d1f996fc011cc71c08
JoyMbugua <mbuguajoy52@gmail.com>
2021-08-27 14:10:54 +0300

    added character page render function and template

--- characters/urls.py
+++ characters/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, character_page
 
 urlpatterns = [ 
-  path('', home, name="home")
+  path('', home, name="home"),
+  path('<int:id>', character_page, name="character_page")
 ]
--- characters/views.py
+++ characters/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .req import get_characters
+from .req import get_characters, get_character_details, get_character_comics
+
 # Create your views here.
 def home(request):
   characters = get_characters()
   return render(request, 'home.html', {'characters': characters})
 
 
+def character_page(request, id):
+  character_info = get_character_details(id)
+  character_comics = get_character_comics(id)
+  context = {
+    'character': character_info,
+    'comics': character_comics,
+    }
+  return render(request, 'character_page.html', context)
 
 
