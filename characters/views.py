1ae26447cf2004cb3552ba58c1f58fb4c53f9e76
JoyMbugua <mbuguajoy52@gmail.com>
2021-08-30 16:39:42 +0300

    added pagination

--- characters/views.py
+++ characters/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator
 from .req import get_characters, get_character_details, get_character_comics, search_hero
 import time 
 
 
 async def home(request):
   characters = await get_characters()
+  characters = Paginator(characters, 6)
+  page_num = request.GET.get('page', 1)
+  try:
+    page = characters.page(page_num)
+  except EmptyPage:
+    page = characters.page(1)
 
-  return render(request, 'characters/home.html', {'characters': characters})
+  return render(request, 'characters/home.html', {'characters': page})
 
 
 async def character_page(request, id):
